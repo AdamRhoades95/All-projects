7f9a227482846d0dfc4335d459f090b82ba5b6a1
AdamRhoades95 <74215378+AdamRhoades95@users.noreply.github.com>
2020-12-07 17:02:41 -0500

    Add files via upload

---
+++ CurrencyConverterTest/CurrencyConverterTest.java
@@ -0,0 +1,15 @@
+package CurrencyConverterTest;
+
+import javax.swing.JFrame;
+
+public class CurrencyConverterTest {
+
+	public static void main(String[] args) {
+		CurrencyConverterFrame temperatureConverterFrame = new CurrencyConverterFrame(); 
+	      temperatureConverterFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      temperatureConverterFrame.setSize(300, 200); 
+	      temperatureConverterFrame.setVisible(true); 
+
+	}
+
+}
